31888561a36145a11edefc9a436c28494ab27ea6
Aryan Agarwal <aryan23062001@gmail.com>
2022-07-14 01:37:09 +0530

    removed

--- server.js
+++ server.js
@@ -65,17 +65,9 @@ app.post("/api/create-order", async (req, res) => {
   }
 });
 
-const __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend", "build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("Api is running");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT;
